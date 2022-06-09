25dab154ad6d81ac74c5800d4372596959d713c6
masi <masi@tiatechnology.com>
2022-06-09 20:08:55 +0300

    ...

--- src/main/java/lt/bit/products/ui/service/UserService.java
+++ src/main/java/lt/bit/products/ui/service/UserService.java
@@ -1,6 +1,12 @@
 package lt.bit.products.ui.service;
 
+import java.util.List;
+import lt.bit.products.ui.model.User;
+import lt.bit.products.ui.service.domain.UserEntity;
 import lt.bit.products.ui.service.domain.UserRepository;
+import lt.bit.products.ui.service.domain.UserRole;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -9,15 +15,17 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class UserService {
 
   private final UserRepository repository;
+  private final ModelMapper mapper;
 
   boolean authenticated;
 
-  public UserService(UserRepository repository) {
+  public UserService(UserRepository repository, ModelMapper mapper) {
     this.repository = repository;
+    this.mapper = mapper;
   }
 
   public void login(String username, String password) {
-    if (repository.existsByUsernameAndPassword(username, password)) {
+    if (repository.existsByUsernameAndPasswordAndRole(username, password, UserRole.ADMIN)) {
       setAuthenticated(true);
     }
   }
@@ -32,5 +40,12 @@ public class UserService {
 
   private void setAuthenticated(boolean authenticated) {
     this.authenticated = authenticated;
+  }
+
+  public List<User> getUsers() {
+    List<UserEntity> users = repository.findAll();
+    // @formatter:off
+    return mapper.map(users, new TypeToken<List<User>>() {}.getType());
+    // @formatter:on
   }
 }
