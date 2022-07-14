e4b33347ff1c357501380386ca8cf9b77dbd66f2
masi <masi@tiatechnology.com>
2022-07-14 22:15:11 +0300

    ...

--- src/main/java/lt/bit/products/ui/service/error/UserValidator.java
+++ src/main/java/lt/bit/products/ui/service/error/UserValidator.java
@@ -1,5 +1,8 @@
 package lt.bit.products.ui.service.error;
 
+import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 import lt.bit.products.ui.model.User;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -8,7 +11,14 @@ import org.springframework.util.StringUtils;
 public class UserValidator {
 
   public void validate(User user) throws ValidationException {
+    validateFields(user);
     validatePasswords(user.getPassword(), user.getConfirmedPassword(), user.getId() == null);
+  }
+
+  private void validateFields(User user) throws ValidationException {
+    if (isEmpty(user.getUsername()) || isNull(user.getRole()) || isNull(user.getStatus())) {
+      throw new ValidationException(ErrorCode.ALL_FIELDS_REQUIRED);
+    }
   }
 
   private void validatePasswords(String newPassword, String confirmedPassword, boolean newUser)
@@ -16,7 +26,7 @@ public class UserValidator {
 
     if (StringUtils.hasLength(newPassword) && !newPassword.equals(confirmedPassword)) {
       throw new ValidationException(ErrorCode.PASSWORDS_DO_NOT_MATCH);
-    } else if (newUser) {
+    } else if (newUser && !StringUtils.hasLength(newPassword)) {
       throw new ValidationException(ErrorCode.PASSWORD_REQUIRED);
     }
   }
