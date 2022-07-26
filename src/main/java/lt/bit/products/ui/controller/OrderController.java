f3983f0d9cb705b5a0d9e2eafc05398e1dfd008e
masi <masi@tiatechnology.com>
2022-07-26 20:16:29 +0300

    ...

---
+++ src/main/java/lt/bit/products/ui/controller/OrderController.java
@@ -0,0 +1,34 @@
+package lt.bit.products.ui.controller;
+
+import static lt.bit.products.ui.controller.ControllerBase.ADMIN_PATH;
+import static lt.bit.products.ui.controller.OrderController.ORDERS_PATH;
+
+import javax.servlet.http.HttpServletRequest;
+import lt.bit.products.ui.service.OrderService;
+import lt.bit.products.ui.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(ADMIN_PATH + ORDERS_PATH)
+class OrderController extends ControllerBase {
+
+  protected static final String ORDERS_PATH = "/orders";
+  private final OrderService service;
+  private final UserService userService;
+
+  OrderController(OrderService service, UserService userService) {
+    this.service = service;
+    this.userService = userService;
+  }
+
+  @GetMapping
+  String showProducts(Model model, HttpServletRequest request) {
+    if (!userService.isAuthenticated()) {
+      return "login";
+    }
+    return "admin/orderList";
+  }
+}
